2ce63b73a9464f860b40845ff0b65ce985006e30
Matt Phillips <mphillip@adobe.com>
2015-12-04 19:54:43 -0600

    wip

--- app.js
+++ app.js
@@ -11,3 +11,28 @@ var server = app.listen(80, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
+var spark = require('spark');
+console.log("logging into spark");
+spark.login({username: 'mattp.dev@gmail.com', password: 'unsecure'}).then(
+	function(token){
+		console.log('login happy: ', token);
+		setInterval(
+			function() {
+				console.log("boo");
+				spark.publishEvent("weather_update", "4").then(
+					function(data) {
+						if (data.ok) { console.log("Event published succesfully") }
+					},
+					function(err) {
+						console.log("Failed to publish event: " + err)
+					}
+				);
+			}, 
+			10 * 1000
+		);
+	},
+	function(err) {
+		console.log('login sad: ', err);
+	}
+);
